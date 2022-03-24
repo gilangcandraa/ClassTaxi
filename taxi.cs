c0cbec02acfab8f42510612e8dc73a41e3b9b704
gilangcandraa <candragilang571@gmail.com>
2022-03-24 21:22:25 +0700

    upload project

---
+++ taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace drivertaksi
+{
+    public class taxi
+    {
+        public string DriverTaksi { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver name: {0}", DriverTaksi);
+            Console.WriteLine("On duty: {0}", OnDuty);
+            Console.WriteLine("Number of passanger: {0}", NumPassenger);
+        }
+        public void PickUpPassanger()
+        {
+            Console.Write(DriverTaksi);
+            Console.WriteLine(" sedang menjemput penumpang");
+        }
+        public void DropOffPassanger()
+        {
+            Console.Write(DriverTaksi);
+            Console.WriteLine(" selesai mengantar penumpang");
+        }
+
+    }
+}
