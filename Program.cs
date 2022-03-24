003e5cb8178cec8d58850196e1e6170c5f648a4e
gilangcandraa <candragilang571@gmail.com>
2022-03-24 21:14:14 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace drivertaksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi=new taxi();
+
+            taxi.DriverTaksi = "Saeb";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 2;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
